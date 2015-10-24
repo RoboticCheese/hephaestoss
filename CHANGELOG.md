fe3126656264a1956b1ba2059310e4f186ca98f9
Jonathan Hartman <j@p4nt5.com>
2015-10-23 21:36:41 -0700

    Initial commit; a freshly generated gem

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Hephaestoss CHANGELOG
+=====================
+
+v0.0.1 (2015-10-23)
+-------------------
+- Development started!
---
+++ spec/hephaestoss_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Hephaestoss do
+  it 'has a version number' do
+    expect(Hephaestoss::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
